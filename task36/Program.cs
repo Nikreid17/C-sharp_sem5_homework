aaf85c7febb53071ed7c6f4a6c89992fcd22e663
Nickolay <nik5te@yandex.ru>
2023-04-02 13:00:12 +0300

    сумма чисел на нечётных позициях

---
+++ task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    Console.WriteLine($"{sum} ");
+}
+
+int[] array = GetRandomArray(10, -99, 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+ReleaseArray(array);
