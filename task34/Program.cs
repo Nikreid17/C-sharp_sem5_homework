82d46f799fe8bff07bdcaad20905db7db8cd6134
Nickolay <nik5te@yandex.ru>
2023-04-02 12:59:36 +0300

    сумма чётных чисел

---
+++ task34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{count} ");
+}
+
+int[] array = GetRandomArray(10, -999, 1000);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+ReleaseArray(array);
